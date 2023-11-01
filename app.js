c23879c2405fdb0a68c8b689c9d4329461740866
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-11-01 11:25:18 +0500

    js assignment-4

---
+++ app.js
@@ -0,0 +1,10 @@
+// Q1=====================
+// let  myName , myAge , mycollege;
+// Q2===================
+// ligal variables
+// camelcase ,snack case ,any name
+// illigal variables
+// var, let ,cont
+// Q3=================
+// variable name should not be js keyword name
+// js Assignment 4 completed============
